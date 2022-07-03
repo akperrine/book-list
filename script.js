7ffee3a486ed0dbec08acd569c83e5d48c5d0c83
Austin Perrine <akperrine@gmail.com>
2022-07-03 12:14:22 -0600

    book deletion working

--- script.js
+++ script.js
@@ -4,7 +4,58 @@ const inputAuthor = document.querySelector("#input-author");
 const inputImportance = document.querySelector("#input-importance");
 
 const submitBtn = document.querySelector("#submit-btn");
-const deleteBtns = document.querySelectorAll(".delete");
+const deleteBtns = document.querySelectorAll(".btn");
+// Book Class
+
+//UI Class
+
+//Store Class
+
+//Events: Display Book
+
+//Event: Remove a Book
+
+class Book {
+  constructor(title, author, importance) {
+    this.title = title;
+    this.author = author;
+    this.importance = importance;
+  }
+}
+
+// class UI {
+//   updateBookList(newBook) {
+//     const newRow = document.createElement("tr");
+//     newRow.innerHTML = `
+//         <td>${newBook.title}</td>
+//         <td>${newBook.author}</td>
+//         <td>${newBook.importance}</td>
+//         `;
+//     bookList.appendChild(newRow);
+//   }
+
+//   static displayBooks() {
+//     const StoredBooks = [
+//       {
+//         title: "Les-Mis",
+//         author: "Hugo",
+//         order: 1,
+//       },
+//       {
+//         title: "Crime n Pun",
+//         author: "Dost",
+//         order: 2,
+//       },
+//     ];
+//     StoredBooks.forEach((book, i) => {
+//       UI.updateBookList(book);
+//     });
+//   }
+// }
+
+// class Store {}
+
+///////
 
 displayBooks = function (storedBooks) {
   bookList.innerHTML = ``;
@@ -13,10 +64,10 @@ displayBooks = function (storedBooks) {
     const newRow = document.createElement("tr");
 
     newRow.innerHTML = `
-    <td>${book.title}</td>
+    <td class="title">${book.title}</td>
     <td>${book.author}</td>
     <td>${book.importance}</td>
-    <td class="delete"><button class="btn btn-danger btn-sm delete rounded-circle" id="delete-btn"></button></td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
 
     bookList.appendChild(newRow);
@@ -36,6 +87,19 @@ setStoredBooks = function (bookToSave) {
   localStorage.setItem("bookList", JSON.stringify([...bookList, bookToSave]));
 };
 
+deleteStoredBook = function (bookTitle) {
+  const bookList = getStoredBooks();
+  console.log(bookList);
+  console.log(bookTitle);
+
+  bookList.forEach((book, i) => {
+    if (book.title === bookTitle) {
+      bookList.splice(i, 1);
+    }
+  });
+  localStorage.setItem("bookList", JSON.stringify(bookList));
+};
+
 bookSubmit = function () {
   const title = inputTitle.value;
   const author = inputAuthor.value;
@@ -51,12 +115,21 @@ bookSubmit = function () {
   displayBooks(bookList);
 };
 
+deleteBookFromList = function (el) {
+  el.parentElement.parentElement.remove();
+  console.log(
+    el.parentElement.parentElement.querySelector(".title").textContent
+  );
+  title = el.parentElement.parentElement.querySelector(".title").textContent;
+  deleteStoredBook(title);
+};
+
 displayBooks(getStoredBooks());
 
 submitBtn.addEventListener("click", bookSubmit);
 
-deleteBtns.forEach((btn) => {
-  return btn.addEventListener("click", function () {
-    console.log("works");
-  });
+bookList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn")) {
+    deleteBookFromList(e.target);
+  }
 });
